86b2726b6ca5ad83315a22f79bf7ba52491ee0a0
ihzurgnauh <1585836193@qq.com>
2024-05-13 15:12:20 +0800

    chore：update thinking page

--- pages/strange-thinkings.md
+++ pages/strange-thinkings.md
@@ -4,27 +4,5 @@ display: 奇思怪想
 subtitle: 想象是灵魂的眼睛
 ---
 
+<WrapperThingText></WrapperThingText>
 
-<WrapperText time="2023-12-07">
-世界是一只大象，抽象是对世界的鞭挞
-</WrapperText>
-
-<WrapperText time="2023-07-11">
-太好了，今天带伞的时候终于记得下雨了
-</WrapperText>
-
-<WrapperText time="2023-05-14">
-猫的叫声是喵，羊的叫声是咩，那么狐狸的叫声是不是呱？
-</WrapperText>
-
-<WrapperText time="2021-01-06">
-月亮会不会在没有人看它的时候偷懒不亮呢?
-</WrapperText>
-
-<WrapperText time="2020-12-21">
-绵羊在晚上会像人类数绵羊一样数人类吗?
-</WrapperText>
-
-<WrapperText time="2020-10-29"> 
-想到一个奇怪的情况，匹诺曹在撒谎的时候，鼻子会变长。那当匹诺曹说"我的鼻子马上会变长"，的时候，怎么判断他说没说谎?
-</WrapperText>
---
+++ src/components/WrapperThingText.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Masonry from '@yeger/vue-masonry-wall'
+import thinkingData  from '../static/thinking-data.json';
+
+defineProps<{
+  time: string
+}>()
+
+</script>
+
+<template>
+<div class="mt-5">
+  <Masonry :items="thinkingData" :ssr-columns="1" :column-width="300" :gap="16">
+    <template #default="{ item, index }">
+      <div class="border-solid border-1 border-#9999 rounded-md hover:shadow-lg hover:border-dashed transition-all">
+        <div class="thinking-text">
+          <span>{{ item.content }}</span>
+          <br />
+          <span class="text-#999 text-xs">{{ item.date }}</span>
+       </div>
+      </div>
+    </template>
+  </Masonry>
+</div>
+</template>
+
+<style scoped>
+.thinking-text {
+  margin: 1rem;
+}
+</style>
