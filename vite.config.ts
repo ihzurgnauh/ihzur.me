fe208a553913e591d9e91374a30cc715efafdd38
ihzurgnauh <1585836193@qq.com>
2024-06-20 14:34:24 +0800

    chore: add mdit footnote

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
-import { basename, dirname, resolve } from 'node:path'
-import { Buffer } from 'node:buffer'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import fs from 'fs-extra'
 import VueRouter from 'unplugin-vue-router/vite'
@@ -12,18 +11,20 @@ import Markdown from 'unplugin-vue-markdown/vite'
 import Vue from '@vitejs/plugin-vue'
 import matter from 'gray-matter'
 import AutoImport from 'unplugin-auto-import/vite'
-import anchor from 'markdown-it-anchor'
+import Anchor from 'markdown-it-anchor'
 import LinkAttributes from 'markdown-it-link-attributes'
 import UnoCSS from 'unocss/vite'
 import SVG from 'vite-svg-loader'
 import MarkdownItShiki from '@shikijs/markdown-it'
 import { rendererRich, transformerTwoslash } from '@shikijs/twoslash'
 import GitHubAlerts from 'markdown-it-github-alerts'
+import Footnote from 'markdown-it-footnote'
+import { figure } from "@mdit/plugin-figure";
+import { slugify } from './scripts/slugify'
 
 // @ts-expect-error missing types
 import TOC from 'markdown-it-table-of-contents'
-// import sharp from 'sharp'
-import { slugify } from './scripts/slugify'
+
 
 const promises: Promise<any>[] = []
 
@@ -97,9 +98,9 @@ export default defineConfig({
           ],
         }))
 
-        md.use(anchor, {
+        md.use(Anchor, {
           slugify,
-          permalink: anchor.permalink.linkInsideHeader({
+          permalink: Anchor.permalink.linkInsideHeader({
             symbol: '#',
             renderAttrs: () => ({ 'aria-hidden': 'true' }),
           }),
@@ -120,6 +121,10 @@ export default defineConfig({
         })
 
         md.use(GitHubAlerts)
+
+        md.use(Footnote)
+
+        md.use(figure)
       },
       // frontmatterPreprocess(frontmatter, options, id, defaults) {
       //   (() => {
@@ -193,33 +198,3 @@ export default defineConfig({
     formatting: 'minify'
   },
 })
-
-// const ogSVg = fs.readFileSync('./scripts/og-template.svg', 'utf-8')
-
-// async function generateOg(title: string, output: string) {
-//   if (fs.existsSync(output))
-//     return
-
-//   await fs.mkdir(dirname(output), { recursive: true })
-//   // breakline every 25 chars
-//   const lines = title.trim().split(/(.{0,25})(?:\s|$)/g).filter(Boolean)
-
-//   const data: Record<string, string> = {
-//     line1: lines[0],
-//     line2: lines[1],
-//     line3: lines[2],
-//   }
-//   const svg = ogSVg.replace(/\{\{([^}]+)}}/g, (_, name) => data[name] || '')
-
-//   // eslint-disable-next-line no-console
-//   console.log(`Generating ${output}`)
-//   try {
-//     await sharp(Buffer.from(svg))
-//       .resize(1200 * 1.1, 630 * 1.1)
-//       .png()
-//       .toFile(output)
-//   }
-//   catch (e) {
-//     console.error('Failed to generate og image', e)
-//   }
-// }
