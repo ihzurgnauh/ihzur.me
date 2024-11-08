34f79745d4e7b24609e6f387482ed9ab5f962166
ihzurgnauh <1585836193@qq.com>
2024-11-08 18:14:07 +0800

    chore: adjust Card font spacing

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,7 +8,7 @@ defineProps<{
 <template>
   <div style="line-height: 1.75;">
     <div class="border-solid border-1 border-#9999 rounded-md hover:border-dashed transition-all">
-      <div class="m-4">
+      <div class="m-4 tracking-wide">
         <span>{{ item.content }}</span>
         <br/>
         <span class="text-#999 text-xs">{{ item.date }}</span>
